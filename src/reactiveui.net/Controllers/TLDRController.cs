562556f1d3bf4bdd8d2c4f9575dade391897fe3a
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-05-27 12:36:01 +0800

    concept/work in progress

---
+++ src/reactiveui.net/Controllers/TLDRController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace reactiveui.net.Controllers
+{
+    public class TLDRController : Controller
+    {
+        [Route("/tldr")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
